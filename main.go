e6db79a4c62fe8ba304aff4ade67e478c0c773dc
s1ovac <ilya.pereverzev123@gmail.com>
2022-11-16 19:29:17 +0300

    added last oral exercise

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 )
 
-func someAction(v []int8, b int8) {
-	v[0] = 100
-	v = append(v, b)
-}
-
 func main() {
-	var a = []int8{1, 2, 3, 4, 5}
-	someAction(a, 6)
-	fmt.Println(a)
+	slice := []string{"a", "a"}
+	func(slice []string) {
+		slice[0] = "b"
+		slice[1] = "b"
+		fmt.Print(slice)
+	}(slice)
+	fmt.Print(slice)
 }
