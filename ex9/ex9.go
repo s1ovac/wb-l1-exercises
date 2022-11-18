b2b40b2066508af237817634dabd353fb6831bc3
s1ovac <ilya.pereverzev123@gmail.com>
2022-11-18 17:34:10 +0300

    added ex9 solution

---
+++ ex9/ex9.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3}
+	b := []int{1, 2, 3, 4, 5}
+	Combine(a, b)
+}
+
+func Combine(a []int, b []int) {
+	mp := make(map[int]int)
+	resultArr := make([]int, 0)
+	// Добавляем уникальные значения множества А в ключи мапы для дальнейшего поиска
+	for _, v := range a {
+		if _, ok := mp[v]; !ok {
+			mp[v] += 1
+		}
+	}
+	// Инкрементируем повторяющееся ключи, для составления множества
+	for _, v := range b {
+		mp[v] += 1
+	}
+	// Ищем по ключам мапы повторяющееся значения и состовляем результирующий массив
+	for key, value := range mp {
+		if value > 1 {
+			resultArr = append(resultArr, key)
+		}
+	}
+	fmt.Println(resultArr)
+}
