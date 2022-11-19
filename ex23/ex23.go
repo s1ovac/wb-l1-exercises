a9d312b4b0f386e3045db8f1296c8d440a92f3cf
s1ovac <ilya.pereverzev123@gmail.com>
2022-11-19 11:18:19 +0300

    added ex23 solution

---
+++ ex23/ex23.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6}
+	// fmt.Println(RemoveItem(2, slice))
+	fmt.Println(RemoveItemByCopy(2, slice))
+}
+
+func RemoveItem(index int, slice []int) []int {
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func RemoveItemByCopy(index int, slice []int) []int {
+	copy(slice[index:], slice[index+1:])
+	return slice[:len(slice)-1]
+}
