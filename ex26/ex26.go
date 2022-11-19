6e7736d4a843da1cbe00afdec7864af95c9457b1
s1ovac <ilya.pereverzev123@gmail.com>
2022-11-19 11:34:18 +0300

    added ex26 solution

--- ex26/ex26.go
+++ ex26/ex26.go
@@ -1,13 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	test := "abcdef"
-	fmt.Println(Distinct(test))
+	testString := "abcdEfA"
+	if Distinct(testString) {
+		fmt.Printf("String '%s' has only distinct symbols\n", testString)
+		return
+	}
+	fmt.Printf("String '%s' has not distinct symbols\n", testString)
 }
 
 func Distinct(str string) bool {
+	str = strings.ToLower(str)
 	distMap := make(map[string]int)
 	for _, s := range str {
 		distMap[string(s)] += 1
