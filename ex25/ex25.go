8e339a6b660d4b1e5b1d3f6abd1ef5ae20aa1911
s1ovac <ilya.pereverzev123@gmail.com>
2022-11-21 17:12:33 +0300

    added ex25 solution

--- ex25/ex25.go
+++ ex25/ex25.go
@@ -1,10 +1,30 @@
 package main
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
-
+	sleepByTime(time.Second * 5)
+	sleepByContext(time.Second * 5)
 }
 
-func sleep(time.Duration) {
+// Реализация sleep через фукнцию After пакета time, которая возвращает канал, тоже самое, что и NewTimer
+func sleepByTime(tm time.Duration) {
+	fmt.Printf("Sleeping by Timer for %.2f sec.", tm.Seconds())
+	until := time.After(tm)
+	select {
+	case <-until:
+		return
+	}
+}
+
+// Реализация sleep через контекст, который ждет завершения и разблокирует горутину
+func sleepByContext(tm time.Duration) {
+	fmt.Printf("Sleeping by Context for %.2f sec.", tm.Seconds())
+	ctx, _ := context.WithTimeout(context.Background(), tm)
+	<-ctx.Done()
+
 }
